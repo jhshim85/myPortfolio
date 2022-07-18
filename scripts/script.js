385e09e73732738e5fd0d27a934395657f8b0a9f
Jaehyun(Jay) Shim <jaehyunshim85@gmail.com>
2022-07-18 17:07:36 -0600

    added theme switch function

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,9 @@
-const pageBody = document.querySelector("body");
-const theme = document.querySelectorAll("input");
+const pageBody = document.querySelector('body');
+const theme = document.querySelector('.theme__type--dark');
 
 function pageMode() {
-  pageBody.classList.toggle("page__theme--light");
-  pageBody.classList.toggle("page__theme--dark");
+  pageBody.classList.toggle('page__theme--light');
+  pageBody.classList.toggle('page__theme--dark');
 }
 
 theme.addEventListener('click', pageMode);
